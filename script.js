64dbf6960c7317e84e6373df62e280362c3a9af6
Shubham Patel <68313477+ShubhamPatel0736@users.noreply.github.com>
2021-06-27 07:20:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-var a=prompt("Enter UserName");
- var b=prompt("Enter password");
-  	if(a== "Shubham" && b=="Patel")
-  	{
+
   	    window.location.href="start.html";
-  	}
-  	else
-  	{
-  		alert("invalid details");
-  		window.location.href="index.html";
-  	}
+ 
   	
